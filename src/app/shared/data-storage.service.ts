92267b95930069fd3a53144e9a173b1da3347125
nebeinstein <nebeinstein@wpi.edu>
2017-09-06 22:20:57 -0400

    base authentication with firebase implemented (storing and sending a token)

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 
+import { AuthService } from '../auth/auth.service';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) { }
+    constructor(private http: Http,
+                private recipeService: RecipeService,
+                private authService: AuthService) {
+    }
 
     storageRecipes() {
-        return this.http.put('https://ng-recipe-book-9d465.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-9d465.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-9d465.firebaseio.com/recipes.json').map(
+        const token = this.authService.getToken();
+        this.http.get('https://ng-recipe-book-9d465.firebaseio.com/recipes.json?auth=' + token).map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
                 for (let recipe of recipes) {
