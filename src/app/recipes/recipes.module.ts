6b9666896a59502b3bd838a95bb1fe31a19ca130
nebeinstein <nebeinstein@wpi.edu>
2017-09-07 11:31:58 -0400

    registered the routes specific to the recipes feature in the feature module using RecipesRoutingModule

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { RecipesRoutingModule } from './recipes-routing.module';
+
 import { RecipesComponent } from './recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -20,7 +22,8 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
     ],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ]
 })
 export class RecipesModule { }
